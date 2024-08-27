2e8c5da632ce1194012e7b38ce47af936b6225fc
Gladsonsaji <gladson.ms@experionglobal.com>
2024-08-27 19:46:04 +0530

    solved leet code 412

---
+++ Q412/q412.js
@@ -0,0 +1,52 @@
+// 412. Fizz Buzz
+// Easy
+// Topics
+// Companies
+// Given an integer n, return a string array answer (1-indexed) where:
+
+// answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+// answer[i] == "Fizz" if i is divisible by 3.
+// answer[i] == "Buzz" if i is divisible by 5.
+// answer[i] == i (as a string) if none of the above conditions are true.
+ 
+
+// Example 1:
+
+// Input: n = 3
+// Output: ["1","2","Fizz"]
+// Example 2:
+
+// Input: n = 5
+// Output: ["1","2","Fizz","4","Buzz"]
+// Example 3:
+
+// Input: n = 15
+// Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+ 
+
+// Constraints:
+
+// 1 <= n <= 104
+
+
+const getModifiedString=(n)=>{
+    const strings=['Fizz','Buzz']
+    const outputString=[]
+    for(i=1;i<=n;i++){
+        // console.log(i%3==0&&i%5==0,"here",i);
+        if(i%3==0&&i%5==0){
+            outputString.push(`${strings[0]}${strings[1]}`)
+        }
+        else if(i%3==0){
+            outputString.push(strings[0])
+        }else if(i%5==0){
+            outputString.push(strings[1])
+        }else{
+            outputString.push(i.toString())
+        }
+    }
+return outputString
+}
+
+const result =getModifiedString(15)
+console.log(result);
